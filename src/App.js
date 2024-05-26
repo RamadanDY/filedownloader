d48c4196c50dd645e7375302556f137ed443900b
Ramadan <daudalaramadan2@gmail.com>
2024-05-26 20:33:09 +0000

    done

--- src/App.js
+++ src/App.js
@@ -1,25 +1,25 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import axios from "axios";
+import FileDownload from "js-file-download";
 
-function App() {
+const App = () => {
+  const download = (e) => {
+    e.preventDefault();
+    axios({
+      url: "http://localhost:4000",
+      method: "Get",
+      responseType: "blob",
+    }).then((res) => {
+      console.log(res);
+      FileDownload(res.data, "downloaded.png");
+    });
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <button onClick={download}>Download</button>
     </div>
   );
-}
+};
 
 export default App;
